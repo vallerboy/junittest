0e9888e3d6a96c4797314ede9fecf18682d06164
Oskar Polak <oskarpolak@vp.pl>
2017-11-18 10:53:58 +0100

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+    public int add(int i, int i1) {
+        return i + i1;
+    }
+
+    public int remove(int i, int i1) {
+        return i - i1;
+    }
+}
---
+++ src/test/java/CalculatorTests.java
@@ -0,0 +1,44 @@
+import org.junit.*;
+
+public class CalculatorTests {
+
+    private Calculator calculator;
+
+    @BeforeClass // Ta metoda wykona się przed wszystkimi testami
+    public static void setUpBefore() throws Exception{
+
+    }
+
+    @AfterClass // Ta metoda wykona sie po wszystkich testach
+    public static void tearDown() throws  Exception{
+
+    }
+
+    @Before // Ta metoda wykona sie przed kazdym testem
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @After // Ta metoda wykona sie po kazdym tescie
+    public void setDown() {
+
+    }
+
+    @Test
+    public void testAdd() {
+        Assert.assertEquals("Nie 10", 10, calculator.add(5,5));
+        Assert.assertEquals("Nie 0", 0, calculator.add(-5,5));
+        Assert.assertEquals("Nie -10", -10, calculator.add(-5,-5));
+        Assert.assertEquals("Nie -2", -2, calculator.add(Integer.MAX_VALUE,Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void testRemove() {
+        Assert.assertEquals("Nie 0", 0, calculator.remove(5,5));
+        Assert.assertEquals("Nie -10", -10, calculator.remove(-5,5));
+        Assert.assertEquals("Nie 0", 0, calculator.remove(-5,-5));
+        Assert.assertEquals("Nie 0", 0, calculator.remove(Integer.MAX_VALUE,Integer.MAX_VALUE));
+    }
+
+
+}
